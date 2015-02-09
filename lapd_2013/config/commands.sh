b453993bfcbd1d06b6a66e5fd60d31dd8a9fb372
Krijn Mossel <krijnmossel@users.noreply.github.com>
2015-02-09 11:44:55 -0800

    Update commands.sh

--- lapd_2013/config/commands.sh
+++ lapd_2013/config/commands.sh
@@ -1,5 +1,10 @@
+#Start Elasticsearch
 /elasticsearch/bin/elasticsearch &
+
+#Wait 10 seconds
 sleep 10
+
+#Set backup repository to the right path.
 curl -XPUT 'http://localhost:9200/_snapshot/backup' -d '{
     "type": "fs",
     "settings": {
@@ -7,5 +12,9 @@ curl -XPUT 'http://localhost:9200/_snapshot/backup' -d '{
         "compress": true
     }
 }'
+
+#Perform restore of data.
 curl -XPOST "localhost:9200/_snapshot/backup/snapshot/_restore"
+
+#Start Kibana
 /kibana/bin/kibana
