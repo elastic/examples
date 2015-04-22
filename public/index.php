1250144b7c54e498c3972c870188914c358e78b1
Shaunak Kashyap <ycombinator@gmail.com>
2015-04-22 06:39:27 -0700

    Search ingredients and directions too.

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ if (!empty($_REQUEST['q'])) {
     // Setup search query
     $searchParams['index'] = Constants::ES_INDEX; // which index to search
     $searchParams['type']  = Constants::ES_TYPE;  // which type within the index to search
-    $searchParams['body']['query']['multi_match']['fields'] = [ 'name', 'description', 'tags' ]; // which fields within the type to search
+    $searchParams['body']['query']['multi_match']['fields'] = [ 'name', 'description', 'tags', 'ingredients', 'directions' ]; // which fields within the type to search
     $searchParams['body']['query']['multi_match']['query'] = $_REQUEST['q']; // what to search for
 
     // Send search query to Elasticsearch and get results
