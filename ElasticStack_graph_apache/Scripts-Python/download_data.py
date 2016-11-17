b70649a20dbd80f7857b45e0bb51709211bd6320
Dale McDiarmid <dalem@elastic.co>
2016-11-17 10:50:20 +0000

    Initial Graph demo commit

---
+++ ElasticStack_graph_apache/Scripts-Python/download_data.py
@@ -0,0 +1,51 @@
+import datetime,requests,gzip,shutil
+import os
+
+from dateutil.relativedelta import *
+from dateutil import parser
+
+base_url="http://www.secrepo.com/self.logs/%s"
+base_filename="access.log.%s.gz"
+start_date="2015-01-17"
+output_folder="./data"
+#if not specified, we run until today
+end_date= datetime.date.today()
+current_date = parser.parse(start_date)
+
+def download_file(url,filename):
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open(filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+        return filename
+    print("Received %s code for %s"%(r.status_code,url))
+    return None
+
+def extract(filename):
+    print ("Extracting file %s"%filename)
+    with gzip.open(filename, 'rb') as f_in, open(output_folder+'/'+(os.path.splitext(filename)[0]), 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+os.mkdir(output_folder)
+while current_date.date() < end_date:
+    filename=base_filename%current_date.strftime('%Y-%m-%d')
+    url = base_url%filename
+    print("Downloading %s"%url)
+    filename = download_file(url,filename)
+    if filename:
+        extract(filename)
+        os.remove(filename)
+    else:
+        print("Could not download %s. Skipping."%url)
+    current_date = current_date+relativedelta(days=+1)
+
+
+
+
+
+
+
+
+
