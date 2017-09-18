f2ae8b80769b1cadc0800763074ed960cc4ab544
Clement Hussenot <chussenota@gmail.com>
2017-09-18 17:32:26 +0200

    Implement an activate! method to return a random array of 3 fruits

--- game/app/app.rb
+++ game/app/app.rb
@@ -1,7 +1,13 @@
 # encoding: utf-8
 
 class ArmedBandit
+  FRUITS = %w{apple coconut banana avocado cherry fig lime mango pear pomelo}
+
   def call(env)
-    Rack::Response.new "Kibana armed bandit! ready?"
+    Rack::Response.new "Kibana armed bandit! #{activate!}"
+  end
+
+  def activate!
+    Array.new(3) { FRUITS.sample }
   end
 end
