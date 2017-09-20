6fcc9023e69f1d9a76399f8a137d7e9314cfca9b
Clement Hussenot <chussenota@gmail.com>
2017-09-20 21:43:20 +0200

    Remove the report generation and minor ping stress (artillery)

--- artillery/run_artillery.sh
+++ artillery/run_artillery.sh
@@ -11,16 +11,8 @@ runArtillery() {
     artillery run $@
 }
 
-generateReport() {
-    echo "Generating report..."
-    artillery report $TMP_DIR/artillery_report_*.json
-    echo "Archiving report in $OUTPUT_DIRECTORY..."
-    cp $TMP_DIR/artillery_report_*.{html,json} "$OUTPUT_DIRECTORY"
-}
-
 main() {
     runArtillery $@
-    generateReport
 }
 
 main $ARGS
