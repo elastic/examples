a84f22912060095fe6e854c971528558eebb5db4
Omer Kushmaro <omer.kushmaro@elastic.co>
2019-12-16 19:35:54 +0200

    updated to install ece using ansible

--- Cloud Enterprise/Getting Started Examples/aws/terraform/variables.tf
+++ Cloud Enterprise/Getting Started Examples/aws/terraform/variables.tf
@@ -1,40 +1,88 @@
-variable "region" {
-  default = "ap-northeast-1"
-}
+#######################################################
+#### Variables to configure (or be prompted about) ####
+#######################################################
 
-variable "zones" {
-  type    = "list"
-  default = ["b", "c"]
-}
-
+# Your aws profile name
 variable "profile" {
-  description = "Your AWS profile name"
+  description = "Your aws profile"
 }
 
+# Your IP address, so that you will be whitelisted against security groups
 variable "trusted_network" {
-  description = "Network ranges (your IP) that will be allowed administrative access"
+  description = "CIDR formatted IP (<IP Address>/32) or Network that will be allowed administrative access"
 }
 
+# General name tag that will be given to spun up instances
 variable "name" {
   description = "An idenfitying name used for names of cloud resources"
 }
 
-variable "cidr" {
-  default = "10.13.37.0/24"
+#######################################################
+######### Variables you may want to configure #########
+#######################################################
+
+# Region to set up ece in
+variable "region" {
+  default = "us-east-2"
 }
 
+# Desired AZs, must have 3.
+variable "zones" {
+  type    = "list"
+  default = ["a","b","c"]
+}
+
+#######################################################
+#### Editable ECE installation-specific variables #####
+#######################################################
+
+# ECE instances's VPC & Subnet cidr
+variable "cidr" {
+  default = "192.168.100.0/24"
+}
+
+# ECE instance type
 variable "instance_type" {
   default = "t2.xlarge"
 }
 
-variable "user_data" {
-  default = "user_data.sh"
-}
-
-variable "remote_user" {
-  default = "ubuntu"
-}
-
+# Path to your public key, which will be used to log in to ece instances
 variable "public_key" {
   default = "~/.ssh/id_rsa.pub"
+}
+
+# Path to your private key for ssh login to servers
+variable "private_key" {
+  default = "~/.ssh/id_rsa"
+}
+
+# An additional volume that will be used by ECE, and its OS represented name
+variable "secondary_device_name" {
+  default="xvdb"
+}
+
+# 
+variable "secondary_device_size" {
+  default=100
+}
+
+# root device size for the ece instances
+variable "root_device_size" {
+  default=50
+}
+
+# Ece version to be installed by ansible
+# Must be supported by the ansible playbook
+variable "ece-version" {
+  default="2.4.0"
+}
+
+#######################################################
+########## Un-editable Variables ######################
+#######################################################
+
+# User to log in to instances and perform install
+# To change this you'll need to modify current way of fetching the aws AMI
+variable "remote_user" {
+  default = "ubuntu"
 }
