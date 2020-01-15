4a589904cfb11e87012e65b48fd0776ab90675ec
Mike Barretta <mike.barretta@gmail.com>
2020-01-15 10:43:05 -0500

    updated aws terraform configs to support v0.12, changed ec2 instance type to i3, and changed AMI to CentOS 7.

--- Cloud Enterprise/Getting Started Examples/aws/terraform/main.tf
+++ Cloud Enterprise/Getting Started Examples/aws/terraform/main.tf
@@ -1,19 +1,19 @@
+terraform {
+  required_version >= 0.12.19
+}
+
 # Initiate ece installation through ansible playbook
 resource "null_resource" "run-ansible" {
   provisioner "local-exec" {
-    command = "${data.template_file.ansible-install.rendered}"
+    command = data.template_file.ansible-install.rendered
   }
 }
 
- output "ece-instances" {
+output "ece-instances" {
    description = "The public dns of created server instances."
-   value = ["${aws_instance.server.*.public_dns}"]
+   value = [aws_instance.server.*.public_dns]
 }
 
-output "ece-ui-url" {
-   value = "${format("https://%s:12443","${aws_instance.server.0.public_dns}")}"
-}
-
-output "ece-api-url" {
-   value = "${format("https://%s:12343","${aws_instance.server.0.public_dns}")}"
+output "installed-ece-url" {
+   value = format("https://%s:12443",aws_instance.server.0.public_dns)
 }
