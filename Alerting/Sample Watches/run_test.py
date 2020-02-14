5ccf027f08e8b4fbd441b73387e817b50fc8896b
Robin Schneider <robin.schneider@geberit.com>
2020-02-14 11:53:11 +0100

    run_test.py: In case a transform fails the transform input is relevant

--- Alerting/Sample Watches/run_test.py
+++ Alerting/Sample Watches/run_test.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
                 if len(response['watch_record']['result']['actions']) == 0:
                     if response['watch_record']['result']['transform']['status'] == 'failure':
                         print("No actions where taken because transform failed: {}".format(
-                            json.dumps(response['watch_record']['result']['transform'], sort_keys=True, indent=2)
+                            json.dumps(response['watch_record']['result'], sort_keys=True, indent=2)
                         ))
                     else:
                         print("No actions where taken: {}".format(
