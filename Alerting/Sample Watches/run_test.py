52125379bc8e1d580356385a948229a1fd20292c
Dan roscigno <dan@roscigno.com>
2020-08-04 19:42:17 -0400

    added remote cluster support and v7 support

--- Alerting/Sample Watches/run_test.py
+++ Alerting/Sample Watches/run_test.py
@@ -3,22 +3,22 @@ import sys
 __author__ = 'dalem@elastic.co'
 
 import datetime
-from elasticsearch import Elasticsearch
-from elasticsearch_xpack import XPackClient
+from elasticsearch7 import Elasticsearch
+from elasticsearch7.client.ingest import IngestClient
 import argparse
 import json
 
 parser = argparse.ArgumentParser(description='Index Connection Log data into ES with the last event at the current time')
-parser.add_argument('--host',help='host name')
-parser.add_argument('--port',help='port')
 parser.add_argument('--user',help='user')
 parser.add_argument('--password',help='password')
+parser.add_argument('--endpoint',help='endpoint')
+parser.add_argument('--port',help='port')
 parser.add_argument('--protocol',help='protocol')
 parser.add_argument('--test_file',help='test file')
 
-parser.set_defaults(host='localhost',port="9200",protocol="http",test_file='data.json',user='elastic',password='changeme')
+parser.set_defaults(endpoint='localhost',port="9200",protocol="http",test_file='data.json',user='elastic',password='changeme')
 args = parser.parse_args()
-es = Elasticsearch([args.protocol+"://"+args.host+":"+args.port],http_auth=(args.user, args.password))
+es = Elasticsearch([args.protocol+"://"+args.endpoint+":"+args.port],http_auth=(args.user, args.password))
 
 def find_item(list, key):
     for item in list:
@@ -28,7 +28,6 @@ def find_item(list, key):
 
 with open(args.test_file,'r') as test_file:
     test=json.loads(test_file.read())
-    #Load Mapping
     try:
         es.indices.delete(test['index'])
     except:
@@ -36,36 +35,32 @@ with open(args.test_file,'r') as test_file:
         pass
     with open(test['mapping_file'],'r') as mapping_file:
         es.indices.create(index=test["index"],body=json.loads(mapping_file.read()))
-    #Load pipeline if its declared
     params={}
     if "ingest_pipeline_file" in test:
         with open(test['ingest_pipeline_file'],'r') as ingest_pipeline_file:
-            es.index(index="_ingest",doc_type="pipeline",id=test["watch_name"],body=json.loads(ingest_pipeline_file.read()))
+            pipeline=json.loads(ingest_pipeline_file.read())
+            p = IngestClient(es)
+            p.put_pipeline(id=test["watch_name"],body=pipeline)
             params["pipeline"]=test["watch_name"]
-    #Index data
     current_data=last_time=datetime.datetime.utcnow()
     i=0
     time_field = test["time_field"] if "time_field" in test else "@timestamp"
     for event in test['events']:
-        #All offsets in seconds
         event_time=current_data+datetime.timedelta(seconds=int(event['offset'] if 'offset' in event else 0))
         event[time_field]=event_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ') if not time_field in event else event[time_field]
-        es.index(index=test['index'],doc_type=test['type'],body=event,id=event['id'] if "id" in event else i,params=params)
+        es.index(index=test['index'],body=event,id=event['id'] if "id" in event else i,params=params)
         i+=1
     es.indices.refresh(index=test["index"])
-    #Load Scripts
     if 'scripts' in test:
         for script in test['scripts']:
             with open(script['path'], 'r') as script_file:
                 es.put_script(id=script["name"],body=json.loads(script_file.read()))
 
-    #Load Watch and Execute
-    watcher = XPackClient(es).watcher
     with open(test['watch_file'],'r') as watch_file:
         watch=json.loads(watch_file.read())
-        watcher.put_watch(id=test["watch_name"],body=watch)
-        response=watcher.execute_watch(test["watch_name"])
-        #Confirm Matches
+        es.watcher.put_watch(id=test["watch_name"],body=watch)
+        response=es.watcher.execute_watch(id=test["watch_name"])
+
         match = test['match'] if 'match' in test else True
         print("Expected: Watch Condition: %s"%match)
         if not 'condition' in response['watch_record']['result']:
