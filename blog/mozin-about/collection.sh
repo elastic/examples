ea1a21f428663c62d5e9d098704a5702926e211f
Andrew Pease <7442091+peasead@users.noreply.github.com>
2021-07-01 11:55:12 -0500

    updates

--- blog/mozin-about/collection.sh
+++ blog/mozin-about/collection.sh
@@ -4,16 +4,16 @@
 curl -X POST https://threatfox-api.abuse.ch/api/v1/ -d '{ "query": "taginfo", "tag": "Mozi", "limit": 1000 }' > mozi-raw.json
 
 # Local Elasticsearch
-ES_HOST='localhost:9200'
+ES_HOST='http://elastic:password@localhost:9200'
 
 # Elastic Cloud
 # ES_HOST='https://elastic:changeme@abcdef0123456789abcdef0123456789.us-central1.gcp.cloud.es.io:9243'
 
 # Create the Threat Fox Ingest Pipeline
-curl -XPUT ${ES_HOST}/_ingest/pipeline/threatfox-enrichment -d@ingest-node-pipeline.json
+curl -XPUT ${ES_HOST}/_ingest/pipeline/threatfox-enrichment -H 'Content-Type: application/json' -d@ingest-node-pipeline.json
 
 # Creates a new index called 'indicators' with the given settings
-curl -XPUT ${ES_HOST}/indicators -H 'Content-Type: application/json' -d@index-mappings.json
+curl -XPUT ${ES_HOST}/indicators -H 'Content-Type: application/json' -d@index-settings.json
 
 # Ingests raw data from the cURL response of Threat Fox in the file listed, then does a bulk upload to ES
 cat mozi-raw.json | jq -c -r '.data[]' | \
