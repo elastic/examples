1fd1cd4e67e46125a0fa1c405bb14ae6787ec219
Andrew Pease <7442091+peasead@users.noreply.github.com>
2021-07-01 11:40:59 -0500

    Update collection.sh

--- blog/mozin-about/collection.sh
+++ blog/mozin-about/collection.sh
@@ -9,6 +9,9 @@ ES_HOST='localhost:9200'
 # Elastic Cloud
 # ES_HOST='https://elastic:changeme@abcdef0123456789abcdef0123456789.us-central1.gcp.cloud.es.io:9243'
 
+# Create the Threat Fox Ingest Pipeline
+curl -XPUT ${ES_HOST}/_ingest/pipeline/threatfox-enrichment -d@ingest-node-pipeline.json
+
 # Creates a new index called 'indicators' with the given settings
 curl -XPUT ${ES_HOST}/indicators -H 'Content-Type: application/json' -d@index-mappings.json
 
