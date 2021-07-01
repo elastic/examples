8203c3acd9b5107f6ef517efcd6ef2da2cdde89d
Andrew Pease <7442091+peasead@users.noreply.github.com>
2021-07-01 11:36:31 -0500

    Update send-data.sh

--- blog/mozin-about/send-data.sh
+++ blog/mozin-about/send-data.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-ES_HOST='https://elastic:changeme@abcdef0123456789abcdef0123456789.us-central1.gcp.cloud.es.io:9243'
+# Collect Mozi sample data
+curl -X POST https://threatfox-api.abuse.ch/api/v1/ -d '{ "query": "taginfo", "tag": "Mozi", "limit": 1000 }' > mozi-raw.json
+
+# Local Elasticsearch
+ES_HOST='localhost:9200'
+
+# Elastic Cloud
+# ES_HOST='https://elastic:changeme@abcdef0123456789abcdef0123456789.us-central1.gcp.cloud.es.io:9243'
 
 # Creates a new index called 'indicators' with the given settings
-curl -XPUT $ES_HOST/indicators -H 'Content-Type: application/json' -d@index-mappings.json
+curl -XPUT ${ES_HOST}/indicators -H 'Content-Type: application/json' -d@index-mappings.json
 
-# Ingests raw data from the curl response of threatfox in the file listed, then does a bulk index to ES
-cat data/mozi-raw.json | jq -c -r '.data[]' | \
+# Ingests raw data from the cURL response of Threat Fox in the file listed, then does a bulk upload to ES
+cat mozi-raw.json | jq -c -r '.data[]' | \
 while read line; do
     echo '{"index":{}}';
     echo $line;
@@ -15,4 +22,4 @@ curl --silent -XPOST \
     -H 'Content-Type: application/x-ndjson' \
     --data-binary \
     @- \
-    $ES_HOST/indicators/_doc/_bulk
+    ${ES_HOST}/indicators/_doc/_bulk
