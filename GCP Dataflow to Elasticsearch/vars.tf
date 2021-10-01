0c0dd54a6c5c01c4621373ba5b9b71ec7227b4f1
Edoardo Tenani <edoardo.tenani@elastic.co>
2021-10-01 18:21:30 +0200

    add GCP Dataflow to Elasticsearch

---
+++ GCP Dataflow to Elasticsearch/vars.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  type        = string
+  description = "The GCP Project ID."
+}
+
+variable "region" {
+  type        = string
+  description = "GCP region to create resources into."
+}
+
